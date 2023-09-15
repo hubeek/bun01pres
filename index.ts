a75425a07a018de6a3c9c7f0382a273fffa2786a
HJ Hubeek <hjhubeek@gmail.com>
2023-09-15 11:09:31 +0200

    ref dom and tsconfig

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
+/*
+* Unfortunately, setting a value for "types" means that TypeScript will ignore other global type definitions, 
+* including lib: ["dom"]. If you need to add DOM types into your project, add the following triple-slash directives at
+*  the top of any TypeScript file in your project.
+* */
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
@@ -6,4 +14,4 @@ const server = Bun.serve({
 });
 
 console.log(`Listening on http://localhost:${server.port} ...`);
-
+console.log(Bun.version)
