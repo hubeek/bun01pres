a5d77eaa6e2601af3ac9788e04b0a807f1593c5e
HJ Hubeek <hjhubeek@gmail.com>
2023-09-15 10:57:48 +0200

    bun serve.

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-import {sayHello} from "./sayHello";
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        return new Response("Bun!");
+    },
+});
 
-console.log("Hello via Bun!");
-const hi = sayHello()
-console.log(hi);
+console.log(`Listening on http://localhost:${server.port} ...`);
+
