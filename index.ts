2f5ae56c6667055b835e6d9f1c2c1916f32c6b12
HJ Hubeek <hjhubeek@gmail.com>
2023-09-18 19:17:59 +0200

    bun presentatie notities toegevoegd aan readme

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
-import { Bun } from "./node_modules/bun-types/types";
+function timelapsed(endTime) {
+    const milliseconds = Math.floor(endTime /1e6)
 
+    package
+    return milliseconds/1000;
+}
 
-console.log(Bun.version)
+function longLoop() {
+    const dl = Bun.nanoseconds()
+    console.log("start")
+    const length = 1000000000
+    for (let i = 0;i < length; i++ ) {
+        const x1 = Math.random()
+    }
+    console.log("end")
+    const timeTaken = timelapsed(Bun.nanoseconds(dl))
+    console.log(`time taken ${timeTaken}s`)
+}
+longLoop()
